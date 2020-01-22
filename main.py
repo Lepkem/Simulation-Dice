3644a11e191aa4fac7c05d7d464f75fc7cf97f5e
Lepkem <Lepkem@users.noreply.github.com>
2020-01-22 13:30:24 +0100

    Bug fix

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from random import randint
 
-def throwdice():
-    die1 = randint(1, 6)
-    die2 = randint(1, 6)
+def throwdice(amount_of_sides=6):
+    die1 = randint(1, amount_of_sides)
+    die2 = randint(1, amount_of_sides)
     sum_die = die1 + die2
     return sum_die
 
@@ -10,11 +10,11 @@ def simulation(func, wished_outcome, runs):
     counter =0 
     i =0
     while i<runs:
-        f = func()
+        f = func
         i+=1
         if f == wished_outcome:
             counter += 1
             
-    return(f'The program was run {runs} times and the amount of favorable outcomes was {wished_outcome}'), (f'\n This means that the chance for this even to happen, is P= {wished_outcome/runs}')
+    return(f'The program was run {runs} times and the amount of favorable outcomes was {wished_outcome}'), (f'\n This means that the chance for this even to happen, is P= {counter/runs}')
 
-print(simulation(throwdice, 7, 100))
+print(simulation(throwdice(12), 7, 100))
